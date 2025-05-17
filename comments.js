1c8d407302ec431372cec894c640403fea7ee7f2
abuyusufalhamdy <abuyusufalhamdy@gmail.com>
2025-05-17 15:17:57 +0300

    Delete comments.js

--- comments.js
+++
@@ -1,7 +0,0 @@
-db.collection('comments').orderBy('date', 'desc').onSnapshot(snapshot => {
-    const comments = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    document.getElementById('comments').innerHTML = comments
-        .filter(comment => !comment.parentId)
-        .map(comment => renderComment(comment))
-        .join('');
-});
